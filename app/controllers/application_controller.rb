850945b0c928326b9251e84c13d1ad41cc15d4da
aoi_takenaka <aaoooii219@gmail.com>
2025-04-13 13:35:59 +0000

    [Update]ログイン・サインアウト後の遷移先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,27 @@ class ApplicationController < ActionController::Base
   # ユーザ登録、ログイン認証時にconfigure_permitted_parameters実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン(サインイン)後にどこに遷移するかを設定するdeviseフックメソッド
+  #
+  # @param resource [Instance] ログインを実行したモデルのデータ、今回の場合はつまりログインしたUserのインスタンス
+  # @return [String] ログイン(サインイン)後にリダイレクトするパス
+  def after_sign_in_path_for(resource)
+    # user_path(current_user.id) TODO後で
+    users_path
+  end
+
+  # サインアウト後にどこに遷移するかを設定しているdeviseフックメソッド
+  # @param param [Instance] アウトを実行したモデルのデータ、今回の場合はつまりログインしたUserのインスタンス
+  # @return [String] サインアウト後にリダイレクトするパス
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
 
-  # サインアップにnameフィールドを有効にする
+  # サインアップにemailフィールドを有効にする
   #
-  # @return [void] 説明
+  # @return [void]
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
