3a9de8205189a98e2c9342d997ede23472a075a0
aoi_takenaka <aaoooii219@gmail.com>
2025-04-13 06:32:16 +0000

    [Fix]ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about", to: "homes#about", as: "about"
-  resources :books, only: [:index, :edit]
-  resources :users, only: [:index, :edit]
+  resources :books, only: [:index, :edit, :show, :create, :destroy]
+  resources :users, only: [:index, :edit, :show, :update]
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
