9f5b03b6b5f369f9137aff1642f7023a51b17ea7
aoi_takenaka <aaoooii219@gmail.com>
2025-04-13 12:39:32 +0000

    [Add]名前でログインできるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # ユーザ登録、ログイン認証時にconfigure_permitted_parameters実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # サインアップにnameフィールドを有効にする
+  #
+  # @return [void] 説明
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
