a9a3bcd305b60dc0d5aa9c80c6245a0ebc207561
aoi_takenaka <aaoooii219@gmail.com>
2025-04-13 06:01:40 +0000

    [Add]booksコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get "homes/about"
-
+  get "homes/about", to: "homes#about", as: "about"
+  resources :books, only: [:index]
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
