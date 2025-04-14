0b16d5bfdd51a828d9a602d5c0880abef4710e55
aoi_takenaka <aaoooii219@gmail.com>
2025-04-14 09:27:36 +0000

    [Fix]左面パーツ移動

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about", to: "homes#about", as: "about"
   
-  # resources :books, only: [:index, :edit, :show, :create, :destroy]
   resources :books, only: [:index, :edit, :create, :show, :update, :destroy]
 
   # resources :users, only: [:index, :edit, :show, :update]
-  resources :users, only: [:index, :edit]
+  resources :users, only: [:index, :edit, :show]
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
