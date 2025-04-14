73aa5718265d12d159546cfa3e9e5b616c749d0c
aoi_takenaka <aaoooii219@gmail.com>
2025-04-14 02:07:02 +0000

    [Update]Book投稿機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(post_book_params)
+    @book.user_id = current_user.id
+
+    if @book.save
+      # redirect_to book_params(@book.user_id)
+      redirect_to users_path
+    else
+      render :index
+    end
   end
 
   def edit
+  end
+
+  private 
+  
+  def post_book_params
+    params.require(:book).permit(:title, :body)
   end
 end
