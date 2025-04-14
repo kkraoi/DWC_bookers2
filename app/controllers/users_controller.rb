30604b0432b82b7da77a580edba0e369258e7e46
aoi_takenaka <aaoooii219@gmail.com>
2025-04-14 10:10:10 +0000

    [Add]User詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books.all
+  end
+
   def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
   get "homes/about", to: "homes#about", as: "about"
   
   resources :books, only: [:index, :edit, :create, :show, :update, :destroy]
 
   # resources :users, only: [:index, :edit, :show, :update]
-  resources :users, only: [:index, :edit]
-  
-  devise_for :users
+  resources :users, only: [:index, :edit, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
