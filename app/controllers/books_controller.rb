be4ef20807459e406cad17dcbb1b38bb567f1f19
aoi_takenaka <aaoooii219@gmail.com>
2025-04-14 09:06:08 +0000

    [Add]Book削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,6 +37,12 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private 
   
   def post_book_params
