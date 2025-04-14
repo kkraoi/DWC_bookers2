ed8ce742668108dfcbd59dd51557a949f9ee0a74
aoi_takenaka <aaoooii219@gmail.com>
2025-04-14 12:10:02 +0000

    [Update]ユーザのプロフィール更新が成功したときフラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
+      flash[:alert] = "You have not updated user."
       render :edit
     end
   end
