6ca532767616a8eb5b1bf3c0972de11ce5bb1b31
aoi_takenaka <aaoooii219@gmail.com>
2025-04-26 04:12:18 +0000

    [Update]ログイン・ロウグアウト・サインアウトのフラッシュメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,23 @@ class ApplicationController < ActionController::Base
   # @param resource [Instance] ログインを実行したモデルのデータ、今回の場合はつまりログインしたUserのインスタンス
   # @return [String] ログイン(サインイン)後にリダイレクトするパス
   def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインに成功しました" # デフォルトのノッティス文言更新
     user_path(current_user.id)
+  end
+
+  # サインアップ後にどこに遷移するかを設定するdeviseフックメソッド
+  #
+  # @param resource [Instance] サインアップを実行したモデルのデータ、今回の場合はつまりサインアップしたUserのインスタンス
+  # @return [String] サインアップ後にリダイレクトするパス
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "サインアップに成功しました"# デフォルトのノッティス文言更新
   end
 
   # サインアウト後にどこに遷移するかを設定しているdeviseフックメソッド
   # @param param [Instance] アウトを実行したモデルのデータ、今回の場合はつまりログインしたUserのインスタンス
   # @return [String] サインアウト後にリダイレクトするパス
   def after_sign_out_path_for(resource)
+    flash[:notice] = "サインアウトに成功しました" # デフォルトのノッティス文言更新
     root_path
   end
 
